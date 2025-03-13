caca7614fd30ece27886fa96a11855c84b3eb783
Ahumaraeze Princewill <princewillahumaraeze@gmail.com>
2025-03-13 13:20:00 +0100

    Added functionality for storing variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
 use std::io::Write;
 
 fn main() {
+    let mut vars: HashMap<String, f64> = HashMap::new();
     println!("Simple Rust Repl, Type exit to quit");
     loop {
         print!(">> ");
@@ -13,8 +15,15 @@ fn main() {
         let input  = input.trim();
         if input.trim() == "exit" {
             break;
+        } else if let Some((var, val)) = input.split_once("="){
+            let var = var.trim();
+            let val: f64 = val.trim().parse().unwrap_or(0.0);
+            vars.insert(var.to_string(), val);
+            println!("Stored {} = {}", var, val);
+        } else if let Some(val) = vars.get(input) {
+            println!("Variable {} = {}", input, val);
+        }else {
+            println!("You entered {}",input);
         }
-
-        println!("You entered {}",input);
     }
 }
