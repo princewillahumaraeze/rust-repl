93752a3512534d16c66d8480fed4eeeff7bae699
Ahumaraeze Princewill <princewillahumaraeze@gmail.com>
2025-03-13 13:06:06 +0100

    Simple Repl Implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Simple Rust Repl, Type exit to quit");
+    loop {
+        print!(">> ");
+        // flush the buffer to ensure prompt is displayed
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        let input  = input.trim();
+        if input.trim() == "exit" {
+            break;
+        }
+
+        println!("You entered {}",input);
+    }
 }
